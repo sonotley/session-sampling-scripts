0deb2b4c76b17bef7c3d8172fd3691973737269d
Simon Notley <43099400+sonotley@users.noreply.github.com>
2025-01-09 10:31:55 +0000

    Working through latency estimation

--- query_length_one_tailed.py
+++ query_length_one_tailed.py
@@ -1,97 +1,153 @@
 import numpy as np
+from matplotlib import pyplot as pt
+import seaborn as sns
 
 from session_sampling_simulator import session_simulator as sim
 import polars as pl
-from milestone_timer import MilestoneTimer
-
-mt = MilestoneTimer()
-print(mt.add_milestone("start"))
 
 DURATION = 3600000
 SAMPLE_PERIOD = 1000
 
 queries = sim.load_queries_from_file("queries.example.yml")
 
-sess = sim.generate_session(queries=queries, window_duration=DURATION)
+results = []
 
-sess = sess // 10  # remove the wait ID for now
+for run in range(1, 100):
+    sess = sim.generate_session(queries=queries, window_duration=DURATION)
 
-true_regions = (
-    pl.DataFrame(
-        sim.find_contiguous_regions(sess),
-        orient="row",
-        schema=["start", "end", "literal"],
+    sess = sess // 10  # remove the wait ID for now
+
+    true_regions = (
+        pl.DataFrame(
+            sim.find_contiguous_regions(sess),
+            orient="row",
+            schema=["start", "end", "literal"],
+        )
+        .with_columns(QID=pl.col("literal") % 100)
+        .with_columns((pl.col("end") - pl.col("start") + 1).alias("true_duration"))
+        .filter(pl.col("QID") != 0)
     )
-    .with_columns(QID=pl.col("literal") % 100)
-    .filter(pl.col("QID") != 0)
-)
-true_regions.write_csv("true.csv")
 
-st = sim.get_sample_times(session_duration=DURATION, sample_period=SAMPLE_PERIOD)
-
-print(mt.add_milestone("generated session"))
-
-sample = sess[st]
-
-query_elapsed_times = zip(
-    st[:-1],
-    sample[:-1],
-    sample[:-1] % 100,
-    sample[:-1] // 100,
-    st[:-1]
-    - (sample[:-1] // 100)
-    + 1,  # Added one to fudge div0 errors, need to think about this
-    sample[1:] % 100,
-)
-
-headers = ["Timestamp", "Raw", "QID", "Start time", "Time since start", "Next QID"]
-
-df = pl.DataFrame(query_elapsed_times, schema=headers)
-df2 = df.filter(pl.col("QID") != 0)
-df2.write_csv("results.csv")
-df3 = (
-    df2.group_by(["Start time", "QID"])
-    .agg(
-        [
-            pl.min("Time since start").alias("a"),
-            pl.min("Timestamp").alias("First"),
-            pl.max("Timestamp").alias("Last"),
-        ]
+    true_regions.write_csv("true.csv")
+    true_summary = true_regions.group_by("QID").agg(
+        pl.mean("true_duration").alias("true_mean")
     )
-    .with_columns((pl.col("Last") - pl.col("First")).alias("c"))
-    .with_columns((2 * pl.col("a") + pl.col("c")).alias("Estimate"))
-    .with_columns(
-        (pl.max_horizontal(SAMPLE_PERIOD / pl.col("Estimate"), 1)).alias("Weight")
-    )
-    .with_columns((pl.col("Estimate") * pl.col("Weight")).alias("weighted_est"))
-    .with_columns((pl.col("Estimate").mean().over("QID")).alias("est_mean"))
-    .with_columns((pl.col("Estimate").var().over("QID")).alias("est_var"))
-    .with_columns((pl.col("a").mean().over("QID")).alias("a_mean"))
-    .with_columns(((16 / 12) * (pl.col("a_mean") ** 2)).alias("exp_var"))
-    # This is all a bit of a random bodge, but it seems to be more or less correct
-    .with_columns(
-        (
-            pl.col("est_mean")
-            + (
-                np.sqrt(1 - pl.col("exp_var") / pl.col("est_var"))
-                * (pl.col("Estimate") - pl.col("est_mean"))
+
+    for phase in np.linspace(0, 0.99, 10):
+        st = sim.get_sample_times(
+            session_duration=DURATION, sample_period=SAMPLE_PERIOD, phase=phase
+        )
+
+        sample = sess[st]
+
+        query_elapsed_times = zip(
+            st[:-1],
+            sample[:-1],
+            sample[:-1] % 100,
+            sample[:-1] // 100,
+            st[:-1]
+            - (sample[:-1] // 100)
+            + 1,  # Added one to fudge div0 errors, need to think about this
+            sample[1:] % 100,
+        )
+
+        headers = ["Timestamp", "Raw", "QID", "start", "Time since start", "Next QID"]
+
+        non_zero_samples = pl.DataFrame(query_elapsed_times, schema=headers).filter(pl.col("QID") != 0).join(true_regions, on="start")        # df2.write_csv("results.csv")
+        observed_queries = (
+            non_zero_samples.group_by(["start", "QID", "end"])
+            .agg(
+                [
+                    pl.min("Time since start").alias("a"),
+                    pl.min("Timestamp").alias("First"),
+                    pl.max("Timestamp").alias("Last"),
+                ]
             )
-        ).alias("est_adj")
-    )
-    .with_columns(
-        (pl.max_horizontal(SAMPLE_PERIOD / pl.col("est_adj"), 1)).alias("weight_adj")
-    )
-    .with_columns((pl.col("Estimate") * pl.col("weight_adj")).alias("weighted_est_adj"))
+            .with_columns((pl.col("Last") - pl.col("First")).alias("c"))
+            .with_columns((pl.col("end") - pl.col("start") + 1).alias("true_duration"))
+            .with_columns((2 * pl.col("a") + pl.col("c")).alias("estimate"))
+            .with_columns(
+                (pl.max_horizontal(SAMPLE_PERIOD / pl.col("estimate"), 1)).alias(
+                    "weight"
+                )
+            )
+            .with_columns((pl.col("estimate") * pl.col("weight")).alias("weighted_est"))
+            .with_columns((pl.col("estimate").mean().over("QID")).alias("est_mean"))
+            .with_columns((pl.col("estimate").var().over("QID")).alias("est_var"))
+            .with_columns((pl.col("a").mean().over("QID")).alias("a_mean"))
+            .with_columns(((16 / 12) * (pl.col("a_mean") ** 2)).alias("exp_var"))
+            # This is all a bit of a random bodge, but it seems to be more or less correct
+            .with_columns(
+                (
+                    pl.col("est_mean")
+                    + (
+                        np.sqrt(
+                            1 - pl.col("exp_var") / pl.col("est_var")
+                        )  # unsure if should be sqrt here - the correct answer seems to be between having and not having it
+                        * (pl.col("estimate") - pl.col("est_mean"))
+                    )
+                ).alias("est_adj")
+            )
+            .with_columns(
+                (pl.max_horizontal(SAMPLE_PERIOD / pl.col("est_adj"), 1)).alias(
+                    "weight_adj"
+                )
+            )
+            .with_columns(
+                (pl.col("estimate") * pl.col("weight_adj")).alias("weighted_est_adj")
+            )
+        )
+        # df3.write_csv("results2.csv")
+
+        # print(df3.describe())
+
+        summary_by_qid = (
+            observed_queries.group_by(["QID"])
+            .agg(
+                [
+                    pl.mean("true_duration").alias("true_duration_sampled_only"),
+                    pl.mean("estimate").alias("mean_est_unweighted"),
+                    (pl.sum("weighted_est") / pl.sum("weight")).alias(
+                        "mean_est_weighted"
+                    ),
+                    (pl.sum("weighted_est_adj") / pl.sum("weight_adj")).alias(
+                        "mean_est_weighted_adj"
+                    ),
+                ]
+            )
+            .join(true_summary, on="QID")
+            .with_columns(phase=phase, run=run)
+        )
+
+        # df4.write_csv("summary.csv")
+        results.append(summary_by_qid)
+
+combined_summary = pl.concat(results).with_columns(
+    [
+        (pl.col(x) - pl.col("true_mean")).alias(f"error-{x}")
+        for x in ("mean_est_weighted_adj", "mean_est_weighted", "mean_est_unweighted")
+    ]
 )
-df3.write_csv("results2.csv")
 
-print(df3.describe())
+# combined_summary.write_csv("many.csv")
 
-# when the queries have no variance, the mean(2est) value looks good... but the weighted one is wrong still
-# It looks like my weighting is adding spurious variance where there is none and biasing the estimate
-# I need some way to account for the variance that comes from the random observation... this is getting super complex
+normalized_combined_summary = combined_summary.unpivot(index=["QID", "phase", "run"])
+# df6.write_csv("unpivot.csv")
 
-# todo: add grouping by QID here
-print(df3.select(pl.sum("weighted_est")) / df3.select(pl.sum("Weight")))
-print(df3.select(pl.sum("weighted_est_adj")) / df3.select(pl.sum("weight_adj")))
-# Need to figure out how to use this to reduce the weighting or perhaps just come up with a different weighting scheme based on a post-hoc adjustment?
+
+sns.boxplot(
+    normalized_combined_summary,
+    # x="query_length_in_periods",
+    x="value",
+    y="variable",
+    native_scale=False,
+    whis=(5, 95),
+    fliersize=0,
+    color="#cfe2f3ff",
+    # linecolor=inner_lines,
+    # **hue_params,
+)
+
+pt.show()
+
+# Because we know the true duration of the sampled queries as well as the true duration of all queries we can see if there's any obvious relationship between the two
