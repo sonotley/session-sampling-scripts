fc37346731f5ec267938adaf615a69f6ed5198ef
Simon Notley <43099400+sonotley@users.noreply.github.com>
2025-03-23 20:35:29 +0000

    More funky charts

--- query_length_one_tailed.py
+++ query_length_one_tailed.py
@@ -6,28 +6,32 @@ from session_sampling_simulator import session_simulator as sim
 import polars as pl
 
 
-def make_query(id: int, mean_duration: int, session_proportion: float) -> sim.Query:
+def make_query(id: int, mean_duration: int, duration_spread: int, session_proportion: float) -> sim.Query:
     return sim.Query(
         id=id,
         mean_duration=mean_duration,
         target_periodicity=int(mean_duration / session_proportion),
         wait_state_ratios={1: 1},
-        duration_distribution='exponential',  # my results seem somewhat biased when using uniform, especially when variance is small
-        duration_spread=int(0)  # only used with uniform
+        duration_distribution='lognormal',
+        duration_spread=duration_spread,
     )
 
 
 DURATION = 3600000
 
 queries = [
-    make_query(id=i, mean_duration=i * 40, session_proportion=0.16)
-    for i in range(1, 6)
+    make_query(id=i, mean_duration=80, duration_spread=i*10,session_proportion=0.16)
+    for i in range(0, 9)
 ]
 
 results = []
+all_observed_queries_results = []
+all_true_queries_results = []
 
-for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
-    for run in range(1, 5):
+for sample_period in [1000]:
+# for sample_period in 1000 / np.linspace(start=1, stop=10, num=5):
+
+    for run in range(1, 10):
         sess = sim.generate_session(queries=queries, window_duration=DURATION)
 
         sess = sess // 10  # remove the wait ID for now
@@ -43,7 +47,8 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
             .filter(pl.col("QID") != 0)
         )
 
-        true_regions.write_csv("true.csv")
+        # true_regions.write_csv("true.csv")
+        all_true_queries_results.append(true_regions)
         true_summary = true_regions.group_by("QID").agg(
             [
                 pl.mean("true_duration").alias("true_mean"),
@@ -51,7 +56,7 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
             ]
         )
 
-        for phase in np.linspace(0, 0.99, 10):
+        for phase in np.linspace(0, 0.99, 20):
             st = sim.get_sample_times(
                 session_duration=DURATION, sample_period=sample_period, phase=phase
             )
@@ -65,18 +70,21 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
                 sample[:-1] // 100,
                 st[:-1]
                 - (sample[:-1] // 100)
-                + 1,  # Added one to fudge div0 errors, need to think about this
+                + 0.5,  # Assume query starts at the start of the ms and sample is in the middle
                 sample[1:] % 100,
             )
 
             headers = ["Timestamp", "Raw", "QID", "start", "Time since start", "Next QID"]
 
+            # Select only the observations where the QID was non-zero
+            # i.e. where we've observed a query execution
             non_zero_samples = (
                 pl.DataFrame(query_elapsed_times, schema=headers)
                 .filter(pl.col("QID") != 0)
                 .join(true_regions, on="start")
             )
 
+            # For each sequence of observations of the same execution, calculate the various duration estimates and weights etc
             observed_queries = (
                 non_zero_samples.group_by(["start", "QID", "end", "true_duration"])
                 .agg(
@@ -98,8 +106,8 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
                 .with_columns((pl.col("estimate").var().over("QID")).alias("est_var"))
                 .with_columns((4 * pl.col("a").var().over("QID")).alias("2a_var"))
                 .with_columns((pl.col("a").mean().over("QID")).alias("a_mean"))
+                # .with_columns(pl.lit(40).alias("a_mean"))  # fixme: hacked this to the correct value to see what happened - weirdly seems to make things worse so it seems my approach isn't great
                 .with_columns(((4/3) * pl.col("a_mean") ** 2).alias("exp_var"))
-                # todo: might be better if we defined this as the proportion of variance in 2a that's explained and only applied it to the 2a part of d, trying this below
                 .with_columns(
                     (
                             pl.col("estimate")
@@ -111,11 +119,12 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
                             )
                     ).alias("est_adj")
             )
+
                 .with_columns(
                     (
                             pl.col("a")
                             + (
-                                (1 - np.sqrt(
+                                (1 - np.sqrt(  # todo: this seems slightly better without the sqrt but I can't justify why
                                         1 - pl.min_horizontal(pl.col("exp_var") / (pl.col("2a_var")), 1)
                                     ))
                                     * (pl.col("a_mean") - pl.col("a"))
@@ -141,10 +150,14 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
                     (pl.col("est_adj") * pl.col("weight_adj")).alias("weighted_est_adj2"))
                 .with_columns(
                     (pl.col("estimate") * pl.col("weight_aadj")).alias("weighted_est_aadj"))
+                .with_columns(
+                    (pl.col("estimate_aadj") * pl.col("weight_aadj")).alias("weighted_est_aadj2"))
             )
 
-            observed_queries.write_csv('oq.csv')
+            # observed_queries.write_csv('oq.csv')
+            all_observed_queries_results.append(observed_queries)
 
+            # Aggregate the data by QID by dividing the sum of the weighted means by the sum of the weights
             summary_by_qid = (
                 observed_queries.group_by(["QID"])
                 .agg(
@@ -164,6 +177,10 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
                         (pl.sum("weighted_est_aadj") / pl.sum("weight_aadj")).alias(
                             "mean_est_weighted_aadj"
                         ),
+                        (pl.sum("weighted_est_aadj2") / pl.sum("weight_aadj")).alias(
+                            "mean_est_weighted_aadj2"
+                        )
+                        ,
                     ]
                 )
                 .join(true_summary, on="QID")
@@ -177,12 +194,12 @@ for sample_period in 1000/np.linspace(start=1, stop=10, num=5):
 combined_summary = pl.concat(results).with_columns(
     [
         (pl.col(x) - pl.col("true_mean")).alias(f"error_{x}")
-        for x in ("mean_est_weighted_adj2", "mean_est_weighted_adj", "mean_est_weighted", "mean_est_unweighted", "mean_est_weighted_aadj")
+        for x in ("mean_est_weighted_adj2", "mean_est_weighted_adj", "mean_est_weighted", "mean_est_unweighted", "mean_est_weighted_aadj", "mean_est_weighted_aadj2")
     ]
 ).with_columns(
     [
         (pl.col(x)/pl.col("true_mean")).alias(f"rel{x}")
-        for x in ("error_mean_est_weighted_adj2", "error_mean_est_weighted_adj", "error_mean_est_weighted", "error_mean_est_unweighted", "error_mean_est_weighted_aadj")
+        for x in ("error_mean_est_weighted_adj2", "error_mean_est_weighted_adj", "error_mean_est_weighted", "error_mean_est_unweighted", "error_mean_est_weighted_aadj", "error_mean_est_weighted_aadj2")
     ]
 )
 
@@ -192,49 +209,111 @@ combined_summary.write_csv("combined_summary.csv")
 normalized_combined_summary = combined_summary.unpivot(index=["QID", "phase", "run", "sample_period", "sample_frequency"])
 normalized_combined_summary.write_csv("ncs.csv")
 
+aggregated_summary = combined_summary.group_by(['QID', 'sample_period']).agg(pl.mean("mean_est_weighted_adj"))
+aggregated_summary.write_csv('aggregated_summary.csv')
 
 # todo: note a plot split out by query id or mean duration actually hides some of the bias
 # todo: although saying that those are suppose to represent different queries so I would want to split them in real life
 
-# sns.boxplot(
-#     normalized_combined_summary.filter(pl.col("variable").str.contains("relerror")),
-#     # x="query_length_in_periods",
-#     x="variable",
-#     y="value",
-#     native_scale=False,
-#     whis=(5, 95),
-#     fliersize=0,
-#     color="#cfe2f3ff",
-#     # hue="QID"
-#     # linecolor=inner_lines,
-#     # **hue_params,
-# )
+fig, axes = pt.subplots(2, 2)
+fig2, axes2 = pt.subplots(5, 5)
+
+a = sns.boxplot(
+    normalized_combined_summary.filter(pl.col("variable").str.contains("relerror")),
+    # x="query_length_in_periods",
+    x="variable",
+    y="value",
+    native_scale=False,
+    whis=(5, 95),
+    fliersize=0,
+    # color="#cfe2f3ff",
+    ax=axes[0,0],
+    # hue="QID"
+    # linecolor=inner_lines,
+    # **hue_params,
+)
 
 sns.boxplot(
-    normalized_combined_summary.filter(pl.col("variable")=="relerror_mean_est_weighted_adj"),
+    normalized_combined_summary.filter(pl.col("variable")=="relerror_mean_est_weighted_aadj2"),
+    # x="query_length_in_periods",
+    x="QID",
+    y="value",
+    native_scale=False,
+    whis=(5, 95),
+    fliersize=0,
+    # color="#cfe2f3ff",
+    ax=axes[0,1],
+    # hue="QID"
+    # linecolor=inner_lines,
+    # **hue_params,
+)
+
+sns.boxplot(
+    # normalized_combined_summary.filter(pl.col("variable")=="relerror_mean_est_weighted_adj"),
+    normalized_combined_summary.filter(pl.col("variable") == "relerror_mean_est_weighted_aadj2"),
     # x="query_length_in_periods",
     x="sample_frequency",
     y="value",
     native_scale=True,
     whis=(5, 95),
     fliersize=0,
-    color="#cfe2f3ff",
-    hue="QID"
+    # color="#cfe2f3ff",
+    hue="QID",
+    ax=axes[1,0],
     # linecolor=inner_lines,
     # **hue_params,
 )
+
+sns.boxplot(
+    # normalized_combined_summary.filter(pl.col("variable")=="relerror_mean_est_weighted_adj"),
+    normalized_combined_summary.filter(pl.col("variable").str.contains("relerror_mean_est")),
+    # x="query_length_in_periods",
+    x="QID",
+    y="value",
+    native_scale=True,
+    whis=(5, 95),
+    fliersize=0,
+    # color="#cfe2f3ff",
+    hue="variable",
+    ax=axes[1,1],
+    # linecolor=inner_lines,
+    # **hue_params,
+)
+
+# It's pretty clear from these distributions how much the uniform 2a distribution flattens out the true distribution
+# I'm wondering if trying to deconvolve it might be useful.
+# That's kind of what my aadj method already does, but it doesn't seem fully effective
+all_true_queries = pl.concat(all_true_queries_results)
+all_observed_queries = pl.concat(all_observed_queries_results)
+cp = sns.color_palette("hls", 8)
+for i in range(1,6):
+    c = cp[i-1]
+    sns.histplot(all_true_queries.filter(pl.col("QID")==i), x="true_duration", ax=axes2[0,i-1], binwidth=10, binrange=(0,500), color=c)
+    sns.histplot(all_observed_queries.filter(pl.col("QID") == i), x="true_duration", ax=axes2[1, i-1],
+                 binwidth=10, binrange=(0,500), color=c)
+    sns.histplot(all_observed_queries.filter(pl.col("QID") == i), x="estimate", ax=axes2[2, i-1],
+                 binwidth=10, binrange=(0,500), color=c)
+    sns.histplot(all_observed_queries.filter(pl.col("QID") == i), x="estimate", weights="weight_aadj", ax=axes2[3, i-1],
+                 binwidth=10, binrange=(0,500), color=c)
+    sns.histplot(all_observed_queries.filter(pl.col("QID") == i), x="estimate_aadj", weights="weight_aadj", ax=axes2[4, i - 1],
+                 binwidth=10, binrange=(0,500), color=c)
+
+pt.show()
 
 # print(normalized_combined_summary.group_by(['QID', 'variable']).agg([
 #             pl.mean("value").alias("mean"),
 #             pl.std("value").alias("std"),
 #         ]).filter(pl.col('variable').is_in('true_mean', 'mean_est_unweighted').sort(by='QID'))
 #
+
+a.tick_params(axis='x', labelrotation=90)
+
+axes[0, 0].grid(True, axis='y')
+axes[0, 1].grid(True, axis='y')
+axes[1, 0].grid(True, axis='y')
+axes[1, 1].grid(True, axis='y')
+# pt.tight_layout()
 pt.show()
-
-# todo: pivot in a different way so we can do a scatter of true mean vs estimated mean
-
-# todo: something weird here that I can't quite get my head round, the relerror for unweighted always seems to be about +100% with exponential when queries are much shorter than the sampling period so, perhaps there is a simpler rule I should be using?
-# todo: it also doesn't seem to correspond much to the mean query duration... but that's expected because it's the variance which should drive bias
 
 
 # Because we know the true duration of the sampled queries as well as the true duration of all queries we can see if there's any obvious relationship between the two
